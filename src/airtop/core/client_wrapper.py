637ec58f6eb3742fabd9a681d8b4a0138009c69a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-01-16 18:49:41 +0000

    Release 0.0.29

--- src/airtop/core/client_wrapper.py
+++ src/airtop/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "airtop",
-            "X-Fern-SDK-Version": "0.0.28",
+            "X-Fern-SDK-Version": "0.0.29",
         }
         headers["Authorization"] = f"Bearer {self._get_api_key()}"
         return headers
