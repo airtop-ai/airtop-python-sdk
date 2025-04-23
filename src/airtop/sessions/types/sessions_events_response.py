11b0493107fd9fce3e99f722e6081cbc129d76d7
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-04-23 20:57:38 +0000

    SDK regeneration

--- src/airtop/sessions/types/sessions_events_response.py
+++ src/airtop/sessions/types/sessions_events_response.py
@@ -3,14 +3,34 @@
 from __future__ import annotations
 from ...core.pydantic_utilities import UniversalBaseModel
 import typing
-from ...types.window_event_message import WindowEventMessage
+from ...types.error_message import ErrorMessage
 from ...core.pydantic_utilities import IS_PYDANTIC_V2
 import pydantic
+from ...types.window_event_message import WindowEventMessage
 from ...types.session_event_message import SessionEventMessage
 from ...types.file_event_message import FileEventMessage
 from ...types.captcha_event_message import CaptchaEventMessage
 from ...types.status_message import StatusMessage
-from ...types.error_message import ErrorMessage
+
+
+class SessionsEventsResponse_Error(UniversalBaseModel):
+    """
+    Each oneOf object in the array represents one possible Server Sent Events (SSE) message, serialized as UTF-8 text according to the SSE specification.
+    """
+
+    event: typing.Literal["error"] = "error"
+    data: ErrorMessage
+    id: typing.Optional[int] = None
+    retry: typing.Optional[int] = None
+
+    if IS_PYDANTIC_V2:
+        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
+    else:
+
+        class Config:
+            frozen = True
+            smart_union = True
+            extra = pydantic.Extra.allow
 
 
 class SessionsEventsResponse_WindowEvent(UniversalBaseModel):
@@ -113,31 +133,11 @@ class SessionsEventsResponse_Status(UniversalBaseModel):
             extra = pydantic.Extra.allow
 
 
-class SessionsEventsResponse_Error(UniversalBaseModel):
-    """
-    Each oneOf object in the array represents one possible Server Sent Events (SSE) message, serialized as UTF-8 text according to the SSE specification.
-    """
-
-    event: typing.Literal["error"] = "error"
-    data: ErrorMessage
-    id: typing.Optional[int] = None
-    retry: typing.Optional[int] = None
-
-    if IS_PYDANTIC_V2:
-        model_config: typing.ClassVar[pydantic.ConfigDict] = pydantic.ConfigDict(extra="allow", frozen=True)  # type: ignore # Pydantic v2
-    else:
-
-        class Config:
-            frozen = True
-            smart_union = True
-            extra = pydantic.Extra.allow
-
-
 SessionsEventsResponse = typing.Union[
+    SessionsEventsResponse_Error,
     SessionsEventsResponse_WindowEvent,
     SessionsEventsResponse_SessionEvent,
     SessionsEventsResponse_FileEvent,
     SessionsEventsResponse_CaptchaEvent,
     SessionsEventsResponse_Status,
-    SessionsEventsResponse_Error,
 ]
